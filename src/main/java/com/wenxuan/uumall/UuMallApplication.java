1f40dd5b6a22adfb7acd38ee1c9c6e5b24063b7b
liangjinning <787613320@qq.com>
2019-04-18 10:23:02 +0800

    1

---
+++ src/main/java/com/wenxuan/uumall/UuMallApplication.java
@@ -0,0 +1,13 @@
+package com.wenxuan.uumall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UuMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UuMallApplication.class, args);
+	}
+
+}
